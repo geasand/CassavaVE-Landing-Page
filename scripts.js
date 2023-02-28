e10c779b39060861b19a2c179782ea4e2fef3482
Gea <sandmop39@gmail.com>
2023-02-28 02:47:24 -0400

    adding javascript function to navigate menu sections

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+
+function clickOnMain() {
+    document.getElementById("product").style.display = "flex";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("services").style.display = "none";
+}
+
+function clickOnAbout() {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("product").style.display = "none";
+    document.getElementById("services").style.display = "none";
+}
+
+function clickOnProducts() {
+    document.getElementById("product").style.display = "flex";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("services").style.display = "none";
+}
